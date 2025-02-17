93ccc5970d05484f87ac8c84c880de81d34f0684
Luke Swindale <luke@swinmail.com>
2025-02-17 12:50:59 +1000

    Improved formatting and meeting info
    Included meeting displayname and duration, in addition to speaker name and event duration. Oh and new event on a new line

--- fetchOverride.js
+++ fetchOverride.js
@@ -5,23 +5,38 @@
 		let [resource, config] = args;
 		const response = await originalFetch(resource, config);
 
-		const clone = response.clone();
-
+		const clone = response.clone();		
+		const meetingDisplayName = g_fileInfo.displayName;
+		const meetingDuration = Math.round(JSON.parse(g_fileInfo.MediaServiceFastMetadata).media.duration/1e7/60) + ' minute(s)';
+		
 		if (resource.includes('streamContent')) {
-			clone.json()
-				.then((data) => {
-					const hiddenDiv = document.createElement('div')
-					hiddenDiv.style.display = 'none';
-					hiddenDiv.id = 'transcript-extractor-for-microsoft-stream-hidden-div-with-transcript';
-					hiddenDiv.innerHTML = data.entries
-						.map(x => x.text)
-						.reduce((sum, x) => sum + x, "\n");
+					clone.json()
+						.then((data) => {
+							const hiddenDiv = document.createElement('div')
+							hiddenDiv.style.display = 'none';
+							hiddenDiv.id = 'transcript-extractor-for-microsoft-stream-hidden-div-with-transcript';
 
-					window.document.body.appendChild(hiddenDiv);
-				})
-				.catch((err) => console.error(err));
-		}
+							// Transform transcript entries into formatted text
+							const transcriptText = data.entries
+							.map(entry => {
+								const speakerName = entry.speakerDisplayName;
+								const startOffset = (''+entry.startOffset).replace(/\.[0-9]+/, '');
+								const endOffset = (''+entry.endOffset).replace(/\.[0-9]+/, '');
+								return `${speakerName} (${startOffset} - ${endOffset}):\n${entry.text}\n`;
+							})
+							.join("\n"); // Adds a blank line between entries
 
-		return response;
+							hiddenDiv.innerHTML = 
+							'------------\n' + 
+							meetingDisplayName + ` (${meetingDuration})\n` +
+							'------------\n\n' + 
+							transcriptText;
+
+							window.document.body.appendChild(hiddenDiv);
+						})
+						.catch((err) => console.error(err));
+				}
+
+				return response;
 	};
 })();
